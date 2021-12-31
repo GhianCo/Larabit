b06d5b1a4c89104b724d4c56b0f1b2af91ed41a3
GhianCo <ghiancorx@gmail.com>
2021-12-31 01:59:48 -0500

    cambios para generador de pacckage

---
+++ post-create-project-command.php
@@ -0,0 +1,17 @@
+<?php
+
+echo <<<EOF
+
+\033[32mProyecto creado con exito!\033[37m
+
+Ejecuta los siguientes comandass:
+
+$ cd [path-project]
+
+\033[33mRecuerda!\033[37m
+Configura tus accesos de la base de datos en: '.env'.
+
+EOF;
+
+unlink('.coveralls.yml');
+unlink('post-create-project-command.php');
