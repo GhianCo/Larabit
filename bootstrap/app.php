63349356b04a9a862e1ff1e9a6121f35b1523eb5
GhianCo <ghiancorx@gmail.com>
2022-08-05 23:22:05 -0500

    cambios

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,22 +21,6 @@ $app = new \Slim\App($settings);
 $app->add(new \CorsSlim\CorsSlim());
 $container = $app->getContainer();
 
-//Cors
-
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header('Access-Control-Allow-Methods: PUT, GET, POST, OPTIONS, DELETE');
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    exit(0);
-}
-
 require __DIR__ . '/../src/constants.php';
 
 require __DIR__ . '/../config/dependencies.php';
